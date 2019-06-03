4a37b87669ad773222b40f9318d1281f78c259dd
BaptisteMeynier <baptiste.meynier@gmail.com>
2019-06-03 21:56:43 +0200

    update to 0.16.0

--- src/main/java/org/acme/config/rs/GreetingResource.java
+++ src/main/java/org/acme/config/rs/GreetingResource.java
@@ -1,8 +1,14 @@
 package org.acme.config.rs;
 
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.config.SmallRyeConfigBuilder;
+import io.smallrye.config.SmallRyeConfigProviderResolver;
 import org.acme.config.model.Computer;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.config.spi.ConfigBuilder;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,32 +24,33 @@ import java.util.Set;
 public class GreetingResource {
 
     @ConfigProperty(name = "greeting.static.message")
-    private String STATIC_MESSAGE;
+    String STATIC_MESSAGE;
 
     @ConfigProperty(name = "greeting.message")
-    private String message;
+    String message;
 
     @ConfigProperty(name = "greeting.suffix", defaultValue="!")
-    private String suffix;
+    String suffix;
 
     @ConfigProperty(name = "greeting.name")
-    private Optional<String> name;
+    Optional<String> name;
 
     @ConfigProperty(name="myPets")
-    private Set<String> myPetsSet;
-
+    Set<String> myPetsSet;
+/*
     @ConfigProperty(name = "date.property")
     LocalDate date;
-
+*/
+@Inject
     @ConfigProperty(name="computer")
-    private Computer computer;
-
+    Computer computer;
+/*
     @ConfigProperty(name = "application.address", defaultValue = "10.0.0.1")
     private InetAddress inetAddress;
 
     @ConfigProperty(name = "application.url", defaultValue = "http://localhost")
     URL urlObject;
-
+*/
     @GET
     public String hello() {
         return message + " " + name.orElse("world") + suffix;
@@ -55,12 +62,12 @@ public class GreetingResource {
         return myPetsSet;
     }
 
-    @GET
+  /*  @GET
     @Path("date")
     public LocalDate date() {
         return date;
     }
-
+*/
     @GET
     @Path("date")
     public Computer computer() {
@@ -72,5 +79,15 @@ public class GreetingResource {
     public String staticConfig() {
         return STATIC_MESSAGE;
     }
+
+   /* public void init(){
+    SmallRyeConfigBuilder builder = new SmallRyeConfigBuilder();
+    Config build = builder.addDefaultSources().build();
+    final SmallRyeConfig src = (SmallRyeConfig) builder.addDefaultSources()
+            .addDiscoveredSources().addDiscoveredConverters().build();
+    SmallRyeConfigProviderResolver.instance().registerConfig(src, Thread.currentThread().getContextClassLoader());
+
+}*/
+
 
 }
